ea9e4c87804d2b49c172d52254c1ad6ea41eb05a
luoPan <1923303700@qq.com>
2018-07-30 19:39:32 +0800

    tftp上传文件以及思路图片

--- Tpython3/T_day02/tftp.py
+++ Tpython3/T_day02/tftp.py
@@ -8,8 +8,7 @@ import time
 def main():
     fileName = input("请输入要下载的文件的名字")
     udpSocket = socket(AF_INET, SOCK_DGRAM)
-    requestFileData = struct.pack("!H%dsb5sb" % len(fileName), 1, fileName.encode("utf-8"), 0, "octet".encode("utf-8"),
-                                  0)
+    requestFileData = struct.pack("!H%dsb5sb" % len(fileName), 1, fileName.encode("utf-8"), 0, "octet".encode("utf-8"),0)
     udpSocket.sendto(requestFileData, ("192.168.1.179", 69))
     f = open(fileName, "w")
     num = 0
@@ -19,11 +18,14 @@ def main():
         recvData, serverInfo = responseData
         print("recvData:%s" % recvData)
 
+        """opNum是操作码 3 表示有数据的文件 5 表示文件不存在  返回的是一个元祖 需要根据下标去取值"""
+
         opNum = struct.unpack("!H", recvData[:2])
         print("opNum:%s" % opNum)
         if opNum[0] == 5:
             print("******FILE NOT FOUND******")
             break
+        """ packetNum返回的是块序号"""
         packetNum = struct.unpack("!H", recvData[2:4])
         print(packetNum)
         if opNum[0] == 3:
@@ -32,9 +34,9 @@ def main():
                 num = 0
             if num == packetNum[0]:
                 f.write(recvData[4:].decode("utf-8"))
-                num = packetNum[0]
+                num = packetNum[0]  # 这句代码没必要写 为了程序代码的安全，建议写上
         # 整理ACK的数据包
-        ackData = struct.pack("!HH", 4, packetNum[0])
+        ackData = struct.pack("!HH", 4, packetNum[0])  # 返回当前收到的块序号 让服务器接着发送下一块数据
         udpSocket.sendto(ackData, serverInfo)
         if len(recvData) < 516:
             udpSocket.close()
